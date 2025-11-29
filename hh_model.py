13a1ba449ffded9f5e9dc2014b8a024fb847fe5f
neuro-trigger <carlosandresrio33@hotmail.com>
2025-11-29 16:51:55 -0500

    gates evolution visualization added

--- hh_model.py
+++ hh_model.py
@@ -11,28 +11,28 @@ Author: Carlos Andres Rios Rojas
 """
 
 import numpy as np
-import matplotlib as plot
+import matplotlib.pyplot as plot
 
 """
 Parameter dictionary example. This dictionary contains the original
 Hodgkin-Huxley parameters.
 """
 HH_PARAMETERS = {
-    "E_Na" : 55.0,  # Nernst potential for Na channel (mV)
-    "g_Na" : 40,    # Maximum conductance for Na channel (mS/cm^2)
-    "E_K" : -77.0,  # Nernst potential for K channel (mV)
-    "g_K" : 35,     # Maximum conductance for K channel (mS/cm^2)
-    "E_L : -65.0,   # Nernst potential for leak channel (mV)
-    "g_L" : 0.3,    # Maximum conductance for leak channel (mS/cm^2)
-    "C" : 1         # Membrane capacitancy (pico F/cm^2)
+    'E_Na' : 55.0,  # Nernst potential for Na channel (mV)
+    'g_Na' : 40,    # Maximum conductance for Na channel (mS/cm^2)
+    'E_K' : -77.0,  # Nernst potential for K channel (mV)
+    'g_K' : 35,     # Maximum conductance for K channel (mS/cm^2)
+    'E_L' : -65.0,  # Nernst potential for leak channel (mV)
+    'g_L' : 0.3,    # Maximum conductance for leak channel (mS/cm^2)
+    'C' : 1         # Membrane capacitancy (pico F/cm^2)
 }
 
 def membrane_potential_differential(s, p, i):
-   na_current = p["g_Na"]*(s[1]**3)*s[3]*(s[0] - p["E_Na"])
-   k_current = p["g_K"]*(s[2]**4)*(s[0] - p["E_K"])
-   l_current = p["g_L"]*(s[0] - p["E_L"])
+   na_current = p['g_Na']*(s[1]**3)*s[3]*(s[0] - p['E_Na'])
+   k_current = p['g_K']*(s[2]**4)*(s[0] - p['E_K'])
+   l_current = p['g_L']*(s[0] - p['E_L'])
 
-   return ( i - (na_current + k_current + l_current) ) / p[C]
+   return ( i - (na_current + k_current + l_current) ) / p['C']
 
 def gate_m_differential(s):
     alpha = ( 0.182 * (s[0] + 35) ) / ( 1 - np.exp( -(s[0] + 35)/9 ) )
@@ -40,17 +40,33 @@ def gate_m_differential(s):
 
     return alpha*(1 - s[1]) - beta*s[1]
 
+def gate_n_differential(s):
+    alpha = ( 0.02 * (s[0] - 25) ) / ( 1 - np.exp( -(s[0] - 25)/9 ) )
+    beta = ( -0.002 * (s[0] - 25) ) / ( 1 - np.exp( (s[0] - 25)/9 ) )
+
+    return alpha*(1 - s[2]) - beta*s[2]
+
 def gate_h_differential(s):
     alpha = 0.25 * np.exp( -(s[0] + 90)/12 )
     beta = 0.25 * np.exp( (s[0] + 62)/6 ) / np.exp( (s[0] + 90)/12 )
 
     return alpha*(1 - s[3]) - beta*s[3]
 
-def gate_n_differential(s):
-    alpha = ( 0.02 * (s[0] - 25) ) / ( 1 - np.exp( -(s[0] - 25)/9 ) )
-    beta = ( -0.002 * (s[0] - 25) ) / ( 1 - np.exp( (s[0] - 25)/9 ) )
+def plot_gate_variables(m, n, h, t):
+    plot.figure(figsize=(8, 5))
 
-    return alpha*(1 - s[2]) - beta*s[2]
+    plot.plot(t, n, label='n(t)')
+    plot.plot(t, m, label='m(t)')
+    plot.plot(t, h, label='h(t)')
+
+    plot.xlabel('Time (ms)')
+    plot.ylabel('Gate Value')
+    plot.title('Gate Variable Evolution')
+    plot.legend()
+    plot.grid(True)
+
+    plot.show()
+
 
 
 
